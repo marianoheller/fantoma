ff96c214e0778025327a2f2ff6fc0c0d2e12884b
Mariano Heller <marianofheller@gmail.com>
2020-11-13 10:05:59 -0300

    fileupload

--- src/Components/FileUpload.purs
+++ src/Components/FileUpload.purs
@@ -1,25 +1,28 @@
 module Components.FileUpload (mkFileUpload) where
 
 import Prelude
-import Data.Foldable (for_)
+import Data.Foldable (for_, traverse_)
+import Effect (Effect)
 import React.Basic.DOM as R
 import React.Basic.DOM.Events (currentTarget)
 import React.Basic.Events (handler)
-import React.Basic.Hooks (Component, component, fragment, useState', (/\))
-import React.Basic.Hooks as React
+import React.Basic.Hooks (Component, component, fragment)
 import Web.File.File as File
 import Web.File.FileList as FileList
+import Web.File.Url as Url
 import Web.HTML.HTMLInputElement as HTMLInputElement
 
-mkFileUpload :: Component {}
+type FileUploadProps
+  = { onFileUpload :: String -> Effect Unit }
+
+mkFileUpload :: Component FileUploadProps
 mkFileUpload = do
-  component "FileUploadComponent" \_ -> React.do
-    fileList /\ setFileList <- useState' []
+  component "FileUploadComponent" \{ onFileUpload } -> React.do
     let
       handleChange t =
         for_ (HTMLInputElement.fromEventTarget t) \fileInput -> do
           maybeFiles <- HTMLInputElement.files fileInput
-          for_ maybeFiles (setFileList <<< map File.name <<< FileList.items)
+          for_ maybeFiles (traverse_ (((=<<) onFileUpload) <<< Url.createObjectURL <<< File.toBlob) <<< FileList.item 0)
     pure
       $ fragment
           [ R.input
@@ -27,7 +30,4 @@ mkFileUpload = do
               , accept: ".mp3,audio/*"
               , onChange: handler currentTarget handleChange
               }
-          , R.pre_
-              [ R.text (show fileList)
-              ]
           ]
--- src/Main.purs
+++ src/Main.purs
@@ -5,27 +5,35 @@ import Components.FileUpload (mkFileUpload)
 import Components.Player (mkPlayer)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
+import Effect.Console (log)
 import Effect.Exception (throw)
 import React.Basic.DOM (render)
+import React.Basic.DOM as DOM
+import React.Basic.Hooks (Component, component, useState', (/\))
+import React.Basic.Hooks as React
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (body)
 import Web.HTML.HTMLElement (toElement)
 import Web.HTML.Window (document)
-import React.Basic.Hooks (Component, component)
-import React.Basic.DOM as DOM
 
 url :: String
 url = "https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3"
+
+onFileUpload :: String -> Effect Unit
+onFileUpload blobUrl = do
+  log $ "GOT BLOB: " <> blobUrl
+  pure unit
 
 mkApp :: Component {}
 mkApp = do
   player <- mkPlayer
   fileUpload <- mkFileUpload
   component "Clock" \props -> React.do
+    blobUrl /\ setBlobUrl <- useState' url -- TODO: state shuold be Maybe String
     pure
       $ DOM.div_
-          [ (player { url })
-          , fileUpload {}
+          [ (player { url: blobUrl })
+          , fileUpload { onFileUpload: setBlobUrl }
           ]
 
 main :: Effect Unit
